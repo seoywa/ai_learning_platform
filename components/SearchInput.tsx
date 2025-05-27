b93493bf4e7bab5a88ef67799653c4abf32e801f
seoywa <132980713+seoywa@users.noreply.github.com>
2025-05-27 12:56:03 +0700

    Fix setTimeout

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -12,29 +12,28 @@ const SearchInput = () => {
   const query = searchParams.get("topic") || "";
   const [searchQuery, setSearchQuery] = useState("");
 
-  useEffect(() => {
-    setTimeout(() => {
-      if (searchQuery) {
-        const newUrl = formUrlQuery({
-          params: searchParams.toString(),
-          key: "topic",
-          value: searchQuery,
-        });
+useEffect(() => {
+        const delayDebounceFn = setTimeout(() => {
+            if(searchQuery) {
+                const newUrl = formUrlQuery({
+                    params: searchParams.toString(),
+                    key: "topic",
+                    value: searchQuery,
+                });
 
-        router.push(newUrl, { scroll: false });
-      } else {
-        if (pathname === "/companions") {
-          const newUrl = removeKeysFromUrlQuery({
-            params: searchParams.toString(),
-            keysToRemove: ["topic"],
-          });
+                router.push(newUrl, { scroll: false });
+            } else {
+                if(pathname === '/companions') {
+                    const newUrl = removeKeysFromUrlQuery({
+                        params: searchParams.toString(),
+                        keysToRemove: ["topic"],
+                    });
 
-          router.push(newUrl, { scroll: false });
-        }
-      }
-    }, 500);
-
-  }, [searchQuery, router, searchParams, pathname]);
+                    router.push(newUrl, { scroll: false });
+                }
+            }
+        }, 500)
+    }, [searchQuery, router, searchParams, pathname]);
 
   return (
     <div className="relative border border-black rounded-lg items-center flex gap-2 px-2 py-1 h-fit">
