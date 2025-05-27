c70fd349cb78ba2620389fdd58c574279ea543a0
seoywa <132980713+seoywa@users.noreply.github.com>
2025-05-27 10:03:23 +0700

    Vapi setup and header

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -1,8 +1,52 @@
+import { getCompanion } from '@/lib/actions/companion.actions';
+import { getSubjectColor } from '@/lib/utils';
+import { currentUser } from '@clerk/nextjs/server';
+import Image from 'next/image';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-const CompanionSession = () => {
+interface CompanionSessionPageProps {
+  params: Promise<{id: string}>
+}
+
+// params: url/{id}/ => id
+// searchParams: url?something=value&key1=value1
+
+const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
+  const { id } = await params ;
+  const { name, subject, topic, title, duration } = await getCompanion(id);
+  const user = await currentUser();
+  if (!user) redirect('/sign-in');
+  if (!name) redirect('/companions');
+
   return (
-    <div>CompanionSession</div>
+    <main>
+      <article className='flex rounded-border justify-between p-6 max-md:flex-col'>
+        <div className='flex items-center gap-4'>
+          <div className='size-[72px] flex items-center justify-center rounded-lg max-md:hidden' style={{ backgroundColor: getSubjectColor(subject)}}>
+            <Image src={`/icons/${subject}.svg`} alt={subject} width={35} height={35} />
+          </div>
+
+          <div className='flex flex-col gap-2'>
+            <div className='flex items-center gap-2'>
+              <p className='font-bold text-2xl'>
+                {name}
+              </p>
+              <div className='subject-badge mad-sm:hidden'>
+                {subject}
+              </div>
+              <p className='text-lg'>
+                {topic}
+              </p>
+            </div>
+          </div>
+
+          <div className='items-start text-xl max-md:hidden'>
+            {duration} minutes
+          </div>
+        </div>
+      </article>
+    </main>
   )
 }
 
